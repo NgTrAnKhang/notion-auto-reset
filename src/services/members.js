7368dc82ef61b6ecde3d80ffb6c8b9a1fd901d12
NgTrAnKhang <87280845+NgTrAnKhang@users.noreply.github.com>
2025-10-22 22:56:04 +0700

    update-user

--- src/services/members.js
+++ src/services/members.js
@@ -6,4 +6,5 @@ export const MEMBER_USERS = [
   { name: 'Danh', id: '292d872b-594c-8152-ae38-000244d0abed' },
   { name: 'Huyo1', id: '292d872b-594c-8139-954e-0002159195af' },
   { name: 'Hẻ', id: '292d872b-594c-81b2-8d11-0002bcbe3ba0' },
+  {name:"Khang nhóc",id:'294d872b-594c-81bd-bb7a-0002fdc5cf22'},
 ];
